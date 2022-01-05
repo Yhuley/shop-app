05d9168caebc452b2e0f92586371633b478fd052
Yhuley <yuriihuley@gmail.com>
2022-01-05 23:59:59 +0200

    button component added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
-import { Htag } from "../components"
+import { Htag, Button } from "../components"
 
 export default function Home() {
   return (
-    <Htag tag="h1">hello</Htag>
+    <>
+      <Htag tag="h1">hello</Htag>
+      <Button appearance="primary">click me</Button>
+      <Button appearance="ghost">click me</Button>
+    </>
   )
 }
