ee8e67ebde7cb08981d4bca140ad099fce19fc93
islamboxer <islamboxingdag@gmail.com>
2022-01-11 16:41:02 +0100

    lst updates

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,10 +1,17 @@
 import { FooterProps } from "./Footer.props"
 import styles from "./Footer.module.css"
 import cn from "classnames"
+import { format } from "date-fns"
 
 
-export const Footer = ({...props}: FooterProps): JSX.Element => {
+export const Footer = ({className, ...props}: FooterProps): JSX.Element => {
     return (
-        <div {...props}>footer</div>
+        <footer {...props} className={cn(className, styles.footer)}>
+            <div>
+                Yhuley @ 2021 - {format(new Date(), "yyyy")} all rights reserved
+            </div>
+            <a href="">GitHub</a>
+            <a href="">Linkedin</a>
+        </footer>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react"
+import { GetStaticProps } from "next"
 import { Htag, Button, Paragraph, Tag, Rating } from "../components"
 import { withLayout } from "../layout/Layout"
+import axios from "axios"
+import { MenuItem } from "../interfaces/menu.interface"
 
-function Home() {
+function Home({ menu, firstCategory }: HomeProps): JSX.Element {
 
   const [rating, setRating] = useState(0)
   return (
@@ -13,8 +16,28 @@ function Home() {
       <Paragraph size="large">as</Paragraph>
       <Tag color="green">fdd</Tag>
       <Rating rating={rating} setRating={setRating} isEditable={true}/>
+      {menu.map(m => console.log(m))}
     </>
   )
 }
 
 export default withLayout(Home)
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0
+  const { data: menu } = await axios.post(process.env.NEXT_PUBLIC_DOMAIN + "api/top-page/find", {
+    firstCategory
+  })
+
+  return {
+    props: {
+       menu,
+       firstCategory
+    }
+  }
+}
+
+interface HomeProps extends Record<string, unknown>{
+  menu: MenuItem[]
+  firstCategory: number
+}
